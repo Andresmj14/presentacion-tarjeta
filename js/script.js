459ca881f347394d7e3acc4f70ecfcad86500d3b
Andresmj14 <andresmjs11@gmail.com>
2025-05-08 12:56:28 -0500

    implementacion de barra de busqueda

--- js/script.js
+++ js/script.js
@@ -4,22 +4,63 @@ class BarraNavegacion extends HTMLElement {
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML =        `
             <style>
-                nav {
-                    background-color: #333;
-                    color: white;
-                    padding: 10px;
+                *{
+                    box-sizing: border-box;
+                    margin: 0;
+                    padding: 0;
+                    font-family: open sans;
                 }
-                a {
+                .barra-navegacion {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    background-color: rgb(71, 63, 71);
+                    padding: 20px;
+                    
+                    
+                    
+                }
+               
+                .barra-navegacion input[type="text"] {
+                    width: 25rem;
+                    margin-right: 10px;
+                    padding: 5px;
+                    border: 5px solid #ccc;
+                    border-radius: 3vw;
+                    font-size: 2rem;
+                    
+                    
+                }
+                .input {
+                    width: 200px;
+                    margin-right: 10px;
+                    padding: 5px;
+                    border: 5px solid #ccc;
+                    border-radius: 3vw;
+                }
+                button {
+                    padding: 5px 10px;
+                    background-color:rgb(43, 68, 180);
                     color: white;
-                    text-decoration: none;
-                    margin: 0 15px;
+                    border: none;
+                    border-radius: 4px;
+                    cursor: pointer;
+                    font-size: 1.5rem;
+                    transition: background-color 0.3s ease;
+                }
+                button:focus {
+                    
+                }
+                button:hover {
+                    background-color:rgb(156, 42, 42);
                 }
             </style>
-            <nav>
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#contact">Contact</a>
-            </nav>
+            <div class="barra-navegacion">
+            
+                <input type="text" placeholder="Buscar...">
+                <button>Buscar</button>
+            </div>
+                
         `;
         
     }
